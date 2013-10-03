f1308cec0086bdcd17672bde9defea48e7bc140f
unknown <jcowhig@SEAWDLNLLX1.internal.imsglobal.com>
2013-10-03 09:16:16 -0700

    StringCalculator for 10/3/13. New keyboard really took its toll.

---
+++ StringCalculator/CSharp/100313/StringCalculator/StringCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorLib {
+    public class StringCalculator {
+        public int Add(string values) {
+            return Split(values ?? String.Empty).Select(Parse).Sum();
+        }
+
+        private static string[] Split(string values) {
+            var delimiters = new List<string> { ",", "\n" };
+
+            var customDelimiter = Regex.Match(values, "//(?<custom>.)\n.*").Groups["custom"];
+            if (customDelimiter.Success) {
+                delimiters.Add(customDelimiter.Value);
+            }
+
+            return values.Split(delimiters.ToArray(), StringSplitOptions.None);
+        }
+
+        private static int Parse(string values) {
+            int parsedValue;
+            return Int32.TryParse(values, out parsedValue) ? parsedValue : 0;
+        }
+    }
+}
---
+++ StringCalculator/CSharp/100313/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorLib;
+
+namespace StringCalculatorTests {
+    [TestClass]
+    public class StringCalculatorTests {
+        private StringCalculator c;
+
+        [TestInitialize]
+        public void Initialize() {
+            c = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void Add_ShouldReturnZeroForEmptyValues() {
+            var emptyValues = new string[] { String.Empty, null };
+            var results = emptyValues.Select(v => c.Add(v));
+            foreach (var result in results) {
+                Assert.AreEqual(0, result);
+            }
+        }
+
+        [TestMethod]
+        public void Add_ShouldParseSingleValue() {
+            var result = c.Add("3");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSumDelimitedValues() {
+            var result = c.Add("1, 2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSupportNewlineDelimiters() {
+            var result = c.Add("1,2\n3");
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSupportCustomDelimiters() {
+            var result = c.Add("//;\n1,2;3");
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "negatives not allowed: -2, -4")]
+        public void Add_ShouldThrowForNegatives() {
+            c.Add("1,-2,3,-4");
+        }
+    }
+}
