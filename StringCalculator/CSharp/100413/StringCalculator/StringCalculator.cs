515327e4dce4a0fec116b3c7058836d3fad6fda3
unknown <jcowhig@SEAWDLNLLX1.internal.imsglobal.com>
2013-10-04 08:54:11 -0700

    String calculator kata for 10/4/13. New keyboard usage getting bette:).

---
+++ StringCalculator/CSharp/100413/StringCalculator/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorLib {
+    public class StringCalculator {
+        public int Add(string values) {
+            var numbers = this.Split(values ?? String.Empty)
+                              .Select(Parse);
+
+            var negatives = numbers.Where(n => n < 0);
+            if (negatives.Any()) {
+                throw new ArgumentException("negatives not allowed: " + String.Join(",", negatives));
+            }
+
+            return numbers.Sum();
+        }
+
+        private string[] Split(string values) {
+            var delimiters = new List<string> { ",", "\n" };
+
+            var customDelimiter = Regex.Match(values, "//(?<custom>.)\n.*").Groups["custom"];
+            if (customDelimiter.Success) {
+                delimiters.Add(customDelimiter.Value);
+            }
+
+            return values.Split(delimiters.ToArray(), StringSplitOptions.None);
+        }
+
+        private int Parse(string value) {
+            int parsedValue;
+            return Int32.TryParse(value, out parsedValue) ? parsedValue : 0;
+        }
+    }
+}
---
+++ StringCalculator/CSharp/100413/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorLib;
+
+namespace StringCalculatorTests {
+    [TestClass]
+    public class StringCalculatorTests {
+        private StringCalculator calculator;
+
+        [TestInitialize]
+        public void Initialize() {
+            calculator = new StringCalculator();
+        }
+        
+        [TestMethod]
+        public void Add_ShouldReturnZeroForEmptyValues() {
+            var emptyValues = new string[] { String.Empty, null };
+            foreach (var value in emptyValues) {
+                var result = calculator.Add(value);
+                Assert.AreEqual(0, result);
+            }
+        }
+
+        [TestMethod]
+        public void Add_ShouldParseSingleValue() {
+            var result = calculator.Add("3");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSumDelimitedValues() {
+            var result = calculator.Add("1,2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSupportNewlineDelimiter() {
+            var result = calculator.Add("1,2\n3");
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSupportCustomDelimiter() {
+            var result = calculator.Add("//;\n1,2;3");
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "negatives not allowed: -2,-4")]
+        public void Add_ShouldThrowForNegatives() {
+            var result = calculator.Add("1,-2,3,-4");
+        }
+    }
+}
