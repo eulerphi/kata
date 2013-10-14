d3c0ed7cd018d847d8512fdc3632e841754e9509
Joshua C <jc@jc.com>
2013-10-14 09:30:16 -0700

    StringCalculator for 10/14/13. Using VsVim and new keyboard :).

---
+++ StringCalculator/CSharp/101413/StringCalculator/StringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorLib {
+    public class StringCalculator {
+        public int Add(string values) {
+            var numbers = this.Split(values ?? String.Empty).Select(this.Parse);
+
+            var negatives = numbers.Where(n => n < 0);
+            if (negatives.Any()) {
+                throw new ArgumentException("negatives not allowed: ", String.Join(",", negatives));
+            }
+
+            return numbers.Sum();
+        }
+
+        private string[] Split(string values) {
+            var delimiters = new List<string> { ",", "\n" };
+
+            var custom = Regex.Match(values, "//(.)\n.*").Groups[1];
+            if (custom.Success) {
+                delimiters.Add(custom.Value);
+            }
+
+            return values.Split(delimiters.ToArray(), StringSplitOptions.None);
+        }
+
+        public int Parse(string value) {
+            int parsedValue;
+            return Int32.TryParse(value, out parsedValue) ? parsedValue : 0;
+        }
+    }
+}
