8dcfd0349fdebd7a0b7c84c5016690a1abe67043
unknown <jcowhig@SEAWDLNLLX1.internal.imsglobal.com>
2013-10-22 08:29:53 -0700

    Tomate1 of BankOCR kata.

---
+++ BankOCR/CSharp/BankOCRLib/Parser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOCRLib {
+    public class Parser {
+        private static readonly Dictionary<string, int> stringToNumber;
+
+        static Parser() {
+            stringToNumber = new Dictionary<string, int>();
+
+            var zero = String.Join("\n",
+                " _ ",
+                "| |",
+                "|_|",
+                "   ");
+            stringToNumber.Add(zero, 0);
+
+            var one = String.Join("\n",
+                "   ",
+                "  |",
+                "  |",
+                "   ");
+            stringToNumber.Add(one, 1);
+
+            var two = String.Join("\n",
+                " _ ",
+                " _|",
+                "|_ ",
+                "   ");
+            stringToNumber.Add(two, 2);
+
+        }
+        public int ParseNumber(string input) {
+            return stringToNumber[input];
+        }
+
+        public string GetNumberAtIndex(string input, int index) {
+            var numberWidth = 3;
+            var startColumn = index * 3;
+
+            var lines = input.Split('\n');
+            var sublines = lines.Select(
+                line => line.Substring(startColumn, numberWidth));
+
+            return String.Join("\n", sublines);
+        }
+    }
+}
---
+++ BankOCR/CSharp/BankOCRTests/ParserTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankOCRLib;
+
+namespace BankOCRTests {
+    [TestClass]
+    public class ParserTests {
+        [TestMethod]
+        public void ParserNumber_ShouldParseZero() {
+            //var toParse =
+            //        " _     _  _     _  _  _  _  _ " +
+            //        "| |  | _| _||_||_ |_   ||_||_|" +
+            //        "|_|  ||_  _|  | _||_|  ||_| _|" +
+            //        "                              ";
+            var zero = String.Join("\n",
+                " _ ",
+                "| |",
+                "|_|",
+                "   ");
+
+            var parser = new Parser();
+            var result = parser.ParseNumber(zero);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void ParserNumber_ShouldParseOne() {
+            var one = String.Join("\n",
+                "   ",
+                "  |",
+                "  |",
+                "   ");
+
+            var parser = new Parser();
+            var result = parser.ParseNumber(one);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void ParseNumber_ShouldParseTwo() {
+            var two = String.Join("\n",
+                " _ ",
+                " _|",
+                "|_ ",
+                "   ");
+
+            var parser = new Parser();
+            var result = parser.ParseNumber(two);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void GetNumberAtIndex() {
+            var input = String.Join("\n",
+                " _     _  _     _  _  _  _  _ ",
+                "| |  | _| _||_||_ |_   ||_||_|",
+                "|_|  ||_  _|  | _||_|  ||_| _|",
+                "                              ");
+
+            var parser = new Parser();
+            var result = parser.GetNumberAtIndex(input, 0);
+
+            var zero = String.Join("\n",
+                " _ ",
+                "| |",
+                "|_|",
+                "   ");
+            Assert.AreEqual(zero, result);
+
+            result = parser.GetNumberAtIndex(input, 1);
+            var one = String.Join("\n",
+                "   ",
+                "  |",
+                "  |",
+                "   ");
+            Assert.AreEqual(one, result);
+        }
+    }
+}
