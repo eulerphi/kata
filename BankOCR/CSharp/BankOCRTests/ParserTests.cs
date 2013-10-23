0e4f06783d51c42e79545761bbc8378554c09f69
unknown <jcowhig@SEAWDLNLLX1.internal.imsglobal.com>
2013-10-23 08:58:48 -0700

    Forgot to add the test changes to the previous commit.

--- BankOCR/CSharp/BankOCRTests/ParserTests.cs
+++ BankOCR/CSharp/BankOCRTests/ParserTests.cs
@@ -6,74 +6,15 @@ namespace BankOCRTests {
     [TestClass]
     public class ParserTests {
         [TestMethod]
-        public void ParserNumber_ShouldParseZero() {
-            //var toParse =
-            //        " _     _  _     _  _  _  _  _ " +
-            //        "| |  | _| _||_||_ |_   ||_||_|" +
-            //        "|_|  ||_  _|  | _||_|  ||_| _|" +
-            //        "                              ";
-            var zero = String.Join("\n",
-                " _ ",
-                "| |",
-                "|_|",
-                "   ");
-
-            var parser = new Parser();
-            var result = parser.ParseNumber(zero);
-            Assert.AreEqual(0, result);
-        }
-
-        [TestMethod]
-        public void ParserNumber_ShouldParseOne() {
-            var one = String.Join("\n",
-                "   ",
-                "  |",
-                "  |",
-                "   ");
-
-            var parser = new Parser();
-            var result = parser.ParseNumber(one);
-            Assert.AreEqual(1, result);
-        }
-
-        [TestMethod]
-        public void ParseNumber_ShouldParseTwo() {
-            var two = String.Join("\n",
-                " _ ",
-                " _|",
-                "|_ ",
-                "   ");
-
-            var parser = new Parser();
-            var result = parser.ParseNumber(two);
-            Assert.AreEqual(2, result);
-        }
-
-        [TestMethod]
-        public void GetNumberAtIndex() {
+        public void Parse_AccountLengthInput() {
             var input = String.Join("\n",
                 " _     _  _     _  _  _  _  _ ",
                 "| |  | _| _||_||_ |_   ||_||_|",
                 "|_|  ||_  _|  | _||_|  ||_| _|",
                 "                              ");
 
-            var parser = new Parser();
-            var result = parser.GetNumberAtIndex(input, 0);
-
-            var zero = String.Join("\n",
-                " _ ",
-                "| |",
-                "|_|",
-                "   ");
-            Assert.AreEqual(zero, result);
-
-            result = parser.GetNumberAtIndex(input, 1);
-            var one = String.Join("\n",
-                "   ",
-                "  |",
-                "  |",
-                "   ");
-            Assert.AreEqual(one, result);
+            var result = new OcrNumberParser().Parse(input);
+            Assert.AreEqual("0123456789", result);
         }
     }
 }
